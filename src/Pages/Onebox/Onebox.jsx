60602a7e970697a70153403ff67d1f769293b72d
pradipk1 <pradipshukla12699@gmail.com>
2024-04-01 05:58:02 +0530

    empty content card done!

--- src/Pages/Onebox/Onebox.jsx
+++ src/Pages/Onebox/Onebox.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Onebox.css';
 import SideNav from '../../components/SideNav/SideNav';
 import TopNav from '../../components/TopNav/TopNav';
+import EmptyContent from '../../components/EmptyContent/EmptyContent';
 
 function Onebox() {
   return (
     <div className='oneboxMainCont'>
       <SideNav />
       <TopNav />
+      <EmptyContent />
     </div>
   )
 }
--- src/components/EmptyContent/EmptyContent.jsx
+++ src/components/EmptyContent/EmptyContent.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './EmptyContent.css';
+import emptyContentImg from '../../Images/emptyContentImg.png';
 
 function EmptyContent() {
   return (
-    <div className='emptyContentMainCont'>EmptyContent</div>
+    <div className='emptyContentMainCont'>
+        <div className='emptyContentCont'>
+            <div className='emptyContentImgCont'><img src={emptyContentImg} alt="emptyContentImg" /></div>
+            <div className='emptyContentTextCont'>
+                <h3>It’s the beginning of a legendary sales pipeline </h3>
+                <p>When you have inbound E-mails <br /> you’ll see them here</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
